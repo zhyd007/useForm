73126e209613d7007b0121f740eb14ce8fbc5b98
007tom <zhyd007@gmail.com>
2021-10-07 10:20:12 +0800

    test: added patternMismatch test

--- src/hooks/useForm/useForm.test.tsx
+++ src/hooks/useForm/useForm.test.tsx
@@ -1,29 +1,39 @@
-import React from "react";
+import React, { FC } from "react";
 import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
-import { Form, TextField } from "../../components";
+import { Form, TextField, Select } from "../../components";
 import type { TextFieldProps } from "../../components";
 import { useForm } from "./useForm";
 
-const TestingComponent = () => {
-  const { values, errors, onChange, handleSubmit } = useForm<
-    Record<"username", string>
-  >({
-    username: "",
-  });
+enum ChildrenType {
+  TextField,
+  Select,
+}
 
-  const formState: Record<"username", Omit<TextFieldProps, "onChange">> = {
-    username: {
-      required: true,
-      label: "Your name",
-      minLength: 8,
-      // pattern: "/[0-9]/",
-      type: "text",
-      name: "username",
-      value: values.username,
-      error: errors.username,
-    },
+type TestingComponentProps = {
+  init: any;
+  recordKey: "username" | "select";
+  childrenType: ChildrenType;
+  opts: {
+    label: string;
+    name: string;
+    minLength?: number;
+    pattern?: string;
+    required?: boolean;
   };
+  selectOpts?: any;
+};
+
+const TestingComponent: FC<TestingComponentProps> = ({
+  init,
+  recordKey,
+  childrenType,
+  opts,
+  selectOpts = {},
+}) => {
+  const { values, errors, onChange, handleSubmit } = useForm<
+    Record<typeof recordKey, string>
+  >({ ...init });
 
   const onSubmit = () => {
     alert(JSON.stringify(values));
@@ -32,12 +42,28 @@ const TestingComponent = () => {
   return (
     <>
       <Form onSubmit={handleSubmit(onSubmit)}>
-        <TextField {...formState.username} onChange={onChange} />
+        {childrenType === ChildrenType.TextField ? (
+          <TextField
+            type="text"
+            value={values.username}
+            error={errors.username}
+            {...opts}
+            onChange={onChange}
+          />
+        ) : childrenType === ChildrenType.Select ? (
+          <Select
+            value={values.select}
+            error={errors.select}
+            options={selectOpts}
+            {...opts}
+            onChange={onChange}
+          />
+        ) : null}
         {/* <TextField {...formState.email} onChange={onChange} />
         <Select {...formState.select} onChange={onChange} />
         <TextField {...formState.password} onChange={onChange} /> */}
         <button type="submit" className="cta-button">
-          next
+          Submit
         </button>
       </Form>
     </>
@@ -46,7 +72,23 @@ const TestingComponent = () => {
 
 describe("useForm", () => {
   it("renders inital form", () => {
-    render(<TestingComponent />);
+    const key = "username";
+
+    const opts: Pick<TextFieldProps, "label" | "name"> = {
+      // required: true,
+      label: "Your name",
+      // minLength: 8,
+      // pattern: "/[0-9]/",
+      name: key,
+    };
+    render(
+      <TestingComponent
+        recordKey="username"
+        init={{ [key]: "" }}
+        childrenType={ChildrenType.TextField}
+        opts={opts}
+      />
+    );
 
     expect(screen.getByTestId("simple-form")).toBeInTheDocument();
   });
@@ -54,7 +96,23 @@ describe("useForm", () => {
   describe("values", () => {
     describe("TextField", () => {
       it("displays values when typing", () => {
-        render(<TestingComponent />);
+        const key = "username";
+
+        const opts: Pick<TextFieldProps, "label" | "name"> = {
+          // required: true,
+          label: "Your name",
+          // minLength: 8,
+          // pattern: "/[0-9]/",
+          name: key,
+        };
+        render(
+          <TestingComponent
+            recordKey="username"
+            init={{ [key]: "" }}
+            childrenType={ChildrenType.TextField}
+            opts={opts}
+          />
+        );
 
         userEvent.type(
           screen.getByTestId(/text-field/i),
@@ -74,39 +132,42 @@ describe("useForm", () => {
 
   describe("errors", () => {
     describe("TextField", () => {
-      it("displays valueMissing error when input is required but no values", () => {
-        render(<TestingComponent />);
-
-        // userEvent.type(screen.getByTestId("text-field"), "a");
-
-        // expect(screen.getByTestId("text-field")).toHaveValue("a");
-
-        // expect(screen.getByTestId("text-field-error")).toBeInTheDocument();
-
-        screen.debug();
-      });
+      it.todo(
+        "displays valueMissing error when input is required but no values"
+      );
       it("displays patternMismatch error when input values against its pattern", () => {
-        render(<TestingComponent />);
+        const key = "username";
 
-        // userEvent.type(screen.getByTestId("text-field"), "a");
+        const opts: Pick<
+          TextFieldProps,
+          "label" | "name" | "required" | "pattern"
+        > = {
+          required: true,
+          label: "Your name",
+          // minLength: 8,
+          pattern: "/[0-9]/",
+          name: key,
+        };
+        render(
+          <TestingComponent
+            recordKey="username"
+            init={{ [key]: "" }}
+            childrenType={ChildrenType.TextField}
+            opts={opts}
+          />
+        );
 
-        // expect(screen.getByTestId("text-field")).toHaveValue("a");
+        userEvent.type(screen.getByTestId("text-field"), "a");
 
-        // expect(screen.getByTestId("text-field-error")).toBeInTheDocument();
+        expect(screen.getByTestId("text-field")).toHaveValue("a");
+
+        expect(screen.getByTestId("text-field-error")).toBeInTheDocument();
 
         screen.debug();
       });
-      it("displays tooShort error when the length of input values is less than minLength", () => {
-        render(<TestingComponent />);
-
-        // userEvent.type(screen.getByTestId("text-field"), "a");
-
-        // expect(screen.getByTestId("text-field")).toHaveValue("a");
-
-        // expect(screen.getByTestId("text-field-error")).toBeInTheDocument();
-
-        screen.debug();
-      });
+      it.todo(
+        "displays tooShort error when the length of input values is less than minLength"
+      );
     });
 
     describe("Select", () => {
